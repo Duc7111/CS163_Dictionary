304c2a8eb81987fe2b9b30dec3b1d5e1a5996fa0
Duc7111 <phantrungduc2003@gmail.com>
2022-06-16 07:56:55 +0700

    main plot

---
+++ Main.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    //load data
+
+    do
+    {
+        system("clear");
+        //starting screen
+
+        //options
+
+        //input
+
+        //processing
+        switch(i)
+        {
+            case 0://exist
+            break;
+
+            default://invalid input
+            cout << "Unknow command, please try again";
+        }
+    } while (i != 0);
+    return 0;
+}
