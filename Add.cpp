bca36936c9c8fc9eaf33c488bd9fbf4c9ae547b5
phungptn <ngothaiphung412@gmail.com>
2022-07-12 00:03:24 +0700

    Update Add.cpp

--- Add.cpp
+++ Add.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int AVL::Add(AVL& tree, wstring key, wstring def, string def_dir)
 {
-	//if (tree.search(key)) return -1;
+	if (tree.search(key)) return -1;
 	ofstream fout(def_dir, ios_base::binary | ios_base::app | ios_base::ate);
 	if (!fout.is_open()) return -2;
 	//int d = fout.tellp(), l = def.length() + 1;
