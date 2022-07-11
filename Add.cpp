165f6ab8ffba821759717d5f87f4bd06f63fc5ee
HarryNgo21 <minhvi176@gmail.com>
2022-07-11 22:25:37 +0700

    update Add function

--- Add.cpp
+++ Add.cpp
@@ -1,13 +1,18 @@
 ﻿#include "AVL.h"
-int AVL::Add(AVL& tree, wstring key ,wstring def, wstring dir)
+#include<iostream>
+using namespace std;
+int AVL::Add(AVL& tree, wstring key, wstring def, string def_dir)
 {
-	if(tree.search(key)) return -1;
-	ofstream fout(dir, ios_base::binary | ios_base::app);
-	if(!fout.is_open()) return -2;
+	if (tree.search(key)) return -1;
+	ofstream fout(def_dir, ios_base::binary | ios_base::app | ios_base::ate);
+	if (!fout.is_open()) return -2;
 	int d = fout.tellp(), l = def.length() + 1;
 	fout.write((char*)&l, sizeof(int));
-	fout.write((char*)&def[0], l*sizeof(wchar_t));
+	fout.write((char*)&def[0], l * sizeof(wchar_t));
 	fout.close();
 	tree.insert(key, d);
+	wcout << d << endl;
+	wcout << key << endl;
+	wcout << def << endl;
 	return d;
 }
