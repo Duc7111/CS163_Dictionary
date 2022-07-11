08352f80b764e432f60c87c7766d480db0acb143
phungptn <ngothaiphung412@gmail.com>
2022-07-12 00:02:10 +0700

    Update Add.cpp

--- Add.cpp
+++ Add.cpp
@@ -3,20 +3,16 @@
 using namespace std;
 
 int AVL::Add(AVL& tree, wstring key, wstring def, string def_dir)
-{/*
-	if (tree.search(key)) return -1;
-	ofstream fout(def_dir, ios_base::binary | ios_base::app | ios_base::ate);
-	if (!fout.is_open()) return -2;
-	int d = fout.tellp(), l = def.length() + 1;
-	int numb = 0;
-	fout.write((char*)&numb, sizeof(int));
-	fout.write((char*)&l, sizeof(int));
-	fout.write((char*)&def[0], l * sizeof(wchar_t));
-	fout.close();
-	tree.insert(key, d);
-	wcout << d << endl;
-	wcout << l << endl;
-	wcout << key << endl;
-	wcout << def << endl;
-	return d;*/
+{
+	//if (tree.search(key)) return -1;
+	//ofstream fout(def_dir, ios_base::binary | ios_base::app | ios_base::ate);
+	//if (!fout.is_open()) return -2;
+	//int d = fout.tellp(), l = def.length() + 1;
+	//int numb = 0;
+	//fout.write((char*)&numb, sizeof(int));
+	//fout.write((char*)&l, sizeof(int));
+	//fout.write((char*)&def[0], l * sizeof(wchar_t));
+	//fout.close();
+	//tree.insert(key, d);
+	return d;
 }
