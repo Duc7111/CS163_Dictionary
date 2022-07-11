85cb8d9e52d9cd8854d77ef516342d61a890b962
phungptn <ngothaiphung412@gmail.com>
2022-07-12 00:02:45 +0700

    Update Add.cpp

--- Add.cpp
+++ Add.cpp
@@ -9,7 +9,7 @@ int AVL::Add(AVL& tree, wstring key, wstring def, string def_dir)
 	//if (!fout.is_open()) return -2;
 	//int d = fout.tellp(), l = def.length() + 1;
 	//int numb = 0;
-	//fout.write((char*)&numb, sizeof(int));
+	fout.write((char*)&numb, sizeof(int));
 	//fout.write((char*)&l, sizeof(int));
 	//fout.write((char*)&def[0], l * sizeof(wchar_t));
 	fout.close();
