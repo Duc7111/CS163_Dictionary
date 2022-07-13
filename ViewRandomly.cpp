4c47a76eed36db122f414d4fd959ec5341587cb9
HarryNgo21 <minhvi176@gmail.com>
2022-07-13 20:54:57 +0700

    update Random a word function

--- ViewRandomly.cpp
+++ ViewRandomly.cpp
@@ -1,27 +1,22 @@
 #include "ViewRandomly.h"
 #include "search.h"
-int FindRandom(AVL tree,bNode*root)
+int FindRandom(AVL& tree, bNode*& root)
 {
 	srand(time(NULL));
 	int total;;
 	tree.num_of_words(root, total);
-	int number = rand()%total+1;
+	int number = rand() % total + 1;
 	return number;
 }
-string ViewRandom(bNode *root,int number,int count,AVL tree,string dir)
+bNode* ViewRandom(bNode*& root, int number, int count, AVL& tree, string dir)
 {
 	if (root)
 	{
 		if (number == count)
 		{
-			string str;
-			ifstream fin(dir, ios::binary);
-			fin.seekg(root->d, ios::cur);
-			fin.read((char*)&str, 500);
-			return str;
+			return root;
 		}
-		if (root->left) return ViewRandom(root, number, count + 1, tree, dir);
-		if (root->right) return ViewRandom(root, number, count + 1, tree, dir);
+		if (root->left) ViewRandom(root->left, number, count + 1, tree, dir);
+		if (root->right) ViewRandom(root->right, number, count + 1, tree, dir);
 	}
-	return "";
 }
