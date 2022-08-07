cd4286f1b876330e0889202ed4988cbbb65b46a0
phungptn <ngothaiphung412@gmail.com>
2022-08-08 00:56:56 +0700

    update

--- ViewRandomly.cpp
+++ ViewRandomly.cpp
@@ -1,15 +1,19 @@
 #include "ViewRandomly.h"
 #include "search.h"
-int FindRandom(AVL& tree, bNode*& root)
+int FindRandom(AVL &tree, bNode* &root)
 {
 	srand((int)time(0));
 	int total = 0;
 	tree.num_of_words(root, total);
+<<<<<<< Updated upstream
 	int number = 1 + rand() % (total + 1 - 1);
 	wcout << number << endl;
+=======
+	int number = rand() % (total + 1);
+>>>>>>> Stashed changes
 	return 1 + (1 + (number * rand()) % (total + 1 - 1)) % (total + 1 - 1);
 }
-void ViewRandom(bNode*& root, int number, int& count, AVL& tree, string dir, bNode*& random)
+void ViewRandom(bNode* &root, int number, int &count, AVL &tree, string dir,bNode*&random)
 {
 	if (root)
 	{
@@ -23,7 +27,7 @@ void ViewRandom(bNode*& root, int number, int& count, AVL& tree, string dir, bNo
 			random = root;
 			return;
 		}
-		if (root->left && !random) ViewRandom(root->left, number, count, tree, dir, random);
-		if (root->right && !random) ViewRandom(root->right, number, count, tree, dir, random);
+		if (root->left && !random) ViewRandom(root->left, number, count, tree, dir,random);
+		if (root->right && !random) ViewRandom(root->right, number, count, tree, dir,random);
 	}
 }
